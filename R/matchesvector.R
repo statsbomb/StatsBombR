e32b44a12d89fba3c5b8a18e8ebbba2a14853e16
YamStats <derrick_yam@brown.edu>
2018-05-25 15:39:09 -0400

    Added the matches vector function

---
+++ R/matchesvector.R
@@ -0,0 +1,10 @@
+matchesvector <- function(username, password, season_id, competition_id){
+  competition_id <- competition_id
+  season_id <- season_id
+  matches.url <- paste0("https://data.statsbombservices.com/api/v1/competitions/", competition_id,
+                        "/seasons/", season_id, "/matches")
+  raw.match.api <- GET(url = matches.url, authenticate(username, password))
+  matches.string <- rawToChar(raw.match.api$content)
+  matches <- fromJSON(matches.string, flatten = T)
+  return(matches$match_id)
+}
