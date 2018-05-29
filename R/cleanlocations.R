7933733f3b323bbcf03994c701bfc463f3d02775
YamStats <derrick_yam@brown.edu>
2018-05-29 15:33:47 -0400

    Problems in the clean location function

--- R/cleanlocations.R
+++ R/cleanlocations.R
@@ -16,12 +16,46 @@ cleanlocations <- function(dataframe){
 
   shots.df <- dataframe %>%
     filter(type.name == "Shot")
-  shots.df$shot.end_location.x <- apply(shots.df, 1, x.f)
-  shots.df$shot.end_location.y <- apply(shots.df, 1, y.f)
-  shots.df$shot.end_location.z <- apply(shots.df, 1, z.f)
-  shots.df$shot.end_location.x <- as.numeric(shots.df$shot.end_location.x)
-  shots.df$shot.end_location.y <- as.numeric(shots.df$shot.end_location.y)
-  shots.df$shot.end_location.z <- as.numeric(shots.df$shot.end_location.z)
+
+  shots.df.end <- shots.df %>%
+    select(shot.end_location)
+
+  x.f <- function(x){
+    if(length(x[[1]]) == 0){
+      return(NA)
+    } else {
+      return(x[[1]][1])
+    }
+  }
+
+  y.f <- function(y){
+    if(length(y[[1]]) == 0){
+      return(NA)
+    } else {
+      return(y[[1]][2])
+    }
+  }
+
+  z.f <- function(z){
+    if(length(z[[1]]) < 3){
+      return(NA)
+    } else {
+      return(z[[1]][3])
+    }
+  }
+
+
+  shots.df.end$shot.end_location.x <- apply(shots.df.end, 1, x.f)
+  shots.df.end$shot.end_location.y <- apply(shots.df.end, 1, y.f)
+  shots.df.end$shot.end_location.z <- apply(shots.df.end, 1, z.f)
+  shots.df.end$shot.end_location.x <- as.numeric(shots.df.end$shot.end_location.x)
+  shots.df.end$shot.end_location.y <- as.numeric(shots.df.end$shot.end_location.y)
+  shots.df.end$shot.end_location.z <- as.numeric(shots.df.end$shot.end_location.z)
+
+  shots.df.end <- shots.df.end %>%
+    select(-shot.end_location)
+
+  shots.df <- bind_cols(shots.df, shots.df.end)
 
   rest.df <- dataframe %>%
     filter(type.name != "Shot")
