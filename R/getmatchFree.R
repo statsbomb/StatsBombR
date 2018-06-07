7971a7aa7f27b635b768b711ecfdec90ddef525f
YamStats <derrick_yam@brown.edu>
2018-06-07 14:33:23 +0100

    Error in indexing fixed

--- R/getmatchFree.R
+++ R/getmatchFree.R
@@ -1,12 +1,12 @@
 get.matchFree <- function(Match){
   Events.url <- paste0("https://raw.githubusercontent.com/statsbomb/open-data/master/data/events/",
-                       Match$match_id[i], ".json")
+                       Match$match_id[1], ".json")
   raw.events.api <- GET(url = Events.url)
   events.string <- rawToChar(raw.events.api$content)
   events <- fromJSON(events.string, flatten = T)
-  events <- events %>% mutate(match_id = Match$match_id[i],
-                              competition_id = Match$competition.competition_id[i],
-                              season_id = Match$season.season_id[i])
+  events <- events %>% mutate(match_id = Match$match_id[1],
+                              competition_id = Match$competition.competition_id[1],
+                              season_id = Match$season.season_id[1])
   return(events)
 }
 
