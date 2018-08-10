94ecf773f5d3ed948535484e29cab33b0608cbe7
YamStats <derrick_yam@brown.edu>
2018-08-10 14:26:42 +0100

    update possession info

--- R/possessioninfo.R
+++ R/possessioninfo.R
@@ -1,13 +1,9 @@
 possessioninfo <- function(dataframe){
-  dataframe <- dataframe %>% mutate(ChangeInPoss = ifelse(possession_team.name != lag(possession_team.name, 1), 1,  0))
-  dataframe <- dataframe %>% mutate(ChangeInPoss = ifelse(is.na(ChangeInPoss), 0,  ChangeInPoss))
-  dataframe <- dataframe %>% group_by(match_id) %>% mutate(Possession = cumsum(ChangeInPoss) + 1)
   dataframe <- dataframe %>%
-    group_by(match_id, Possession) %>%
+    group_by(match_id, possession) %>%
     mutate(StartOfPossession = min(ElapsedTime),
            TimeInPoss = ElapsedTime - StartOfPossession,
            TimeToPossEnd = max(ElapsedTime) - ElapsedTime) %>%
-    ungroup() %>%
-    select(-ChangeInPoss)
+    ungroup()
   return(dataframe)
 }
