1648b0ee32aa84a217d61c9bfff6cd8a464277ea
YamStats <derrick_yam@brown.edu>
2019-04-01 15:34:54 -0400

    Removed filtering for event type

--- R/GetPlayerFootedness.R
+++ R/GetPlayerFootedness.R
@@ -4,14 +4,14 @@ GetPlayerFootedness <- function(Passes){
   #That player's use their dominant foot the majority of the time.
   #You can load all of the data, but it must include the type.name and the player id.
   Passes <- Passes %>%
-    filter(type.name == "Pass") %>%
     filter(pass.body_part.name %in% c("Left Foot", "Right Foot"))
 
   Footedness <- Passes %>%
     group_by(player.id) %>%
     summarise(RightFoot = sum(pass.body_part.name == "Right Foot")/n(),
               LeftFoot = sum(pass.body_part.name == "Left Foot")/n(),
-              DominantFoot = ifelse(RightFoot >= LeftFoot, "Right Foot", "Left Foot"))
+              DominantFoot = ifelse(RightFoot >= LeftFoot, "Right Foot", "Left Foot")) %>%
+    ungroup()
 
   #Data frame returned with player id, % right used, % left used, and Dominant Foot name.
   return(Footedness)
