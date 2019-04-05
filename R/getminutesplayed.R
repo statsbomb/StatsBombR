a53cce648c31afe30728e0fab04c39a68bc0947c
YamStats <derrick_yam@brown.edu>
2019-04-05 11:12:30 -0400

    Fixed minutes played function

--- R/getminutesplayed.R
+++ R/getminutesplayed.R
@@ -1,53 +1,82 @@
-###
+get.minutesplayed <- function(eventscleaned){
+  Matches <- eventsclean %>%
+    group_by(match_id) %>%
+    filter(ElapsedTime == max(ElapsedTime)) %>%
+    select(match_id, GameEnd = ElapsedTime) %>%
+    dplyr::slice(1)
 
-get.minutesplayed <- function(events){
+  StartingXI <- eventsclean %>%
+    filter(type.name == "Starting XI") %>%
+    select(id, match_id, team.id, tactics.lineup)
 
-  MatchIDs <- unique(events$match_id)
-  get.minutesplayed.single.match <- function(MatchSelect){
-
-    per.match <- events %>% filter(match_id == MatchSelect)
-    match.length <- max(per.match$minute)
-    line.up1 <- per.match[1,]$tactics.lineup[[1]]
-    line.up1$team.name <- per.match[1,]$team.name
-
-    line.up2 <- per.match[2,]$tactics.lineup[[1]]
-    line.up2$team.name <- per.match[2,]$team.name
-
-    line.ups <- bind_rows(line.up1, line.up2)
-    line.ups <- line.ups %>% select(player.id, player.name, team.name)
-
-    line.ups$time.start <- 0
-
-    substitutions <- per.match %>% filter(type.name == "Substitution") %>% select(minute, team.name, player.name, player.id, substitution.replacement.name)
-    substitutions.out <- substitutions %>% select(-substitution.replacement.name, -team.name, -player.id)
-
-    line.ups <- merge(line.ups, substitutions.out, by = "player.name", all = T)
-
-    substitutions.in <- data.frame(player.name = substitutions$substitution.replacement.name, team.name = substitutions$team.name, time.start = substitutions$minute, minute = match.length, stringsAsFactors = F)
-
-    playerIDS <- per.match %>% group_by(player.name) %>% select(player.name, player.id)
-    playerIDS <- unique(playerIDS)
-    playerIDS <- playerIDS %>% filter(player.name %in% substitutions.in$player.name)
-
-    substitutions.in <- merge(substitutions.in, playerIDS, by = "player.name")
-
-    line.ups <- bind_rows(line.ups, substitutions.in)
-    line.ups[is.na(line.ups)] <- match.length
-    line.ups$Minutes.Played <- line.ups$minute - line.ups$time.start
-
-    line.ups$match_id <- MatchSelect
-    colnames(line.ups) <- c("player.name", "player.id", "team.name", "time.in", "time.out", "minutes.played", "match_id")
-
-    return(line.ups)
-
+  ##Trying a different method for the speed.
+  myList <- StartingXI$tactics.lineup
+  fixnull <- function(x) {
+    if(is.data.frame(x)){
+      return(x)
+    } else {
+      return(setNames(data.frame(matrix(ncol = ncol(myList[[1]]), nrow = 1)), names(myList[[1]])))
+    }
   }
 
-  MinsPlayedCatch <- MatchIDs %>%
-    split(1:length(.)) %>%
-    purrr::map(get.minutesplayed.single.match) %>%
-    dplyr::bind_rows()
+  # Apply the written function above to every element in myList
+  myList <- lapply(myList, fixnull)
 
-  MinsPlayedCatch <- MinsPlayedCatch %>% group_by(match_id, player.id) %>% slice(1)
-  return(MinsPlayedCatch)
+  # "bind_rows" with mynewList
+  df <- bind_rows(myList, .id = "id")
 
+  ##Index Length
+  idtable <- df %>%
+    mutate(id = as.numeric(id)) %>%
+    group_by(id) %>%
+    dplyr::slice(1) %>%
+    select(id) %>%
+    ungroup() %>%
+    mutate(sbid = StartingXI$id) %>%
+    mutate(id = as.character(id)) %>%
+    mutate(match_id = StartingXI$match_id,
+           team.id = StartingXI$team.id)
+
+  #Join with the freeze frame table
+  df <- left_join(df, idtable)
+  df <- df %>% select(-id) %>% rename(id = sbid) %>% select(id, everything())
+
+  df <- df %>%
+    mutate(TimeOn = 0)
+
+  df <- df %>% select(player.id, match_id, team.id, TimeOn)
+
+  #Get Substitutions ignore Player Off and Player On for now.
+  Subs <- eventsclean %>%
+    filter(type.name == "Substitution") %>%
+    select(match_id, ElapsedTime, Off = player.id, team.id, On = substitution.replacement.id) %>%
+    tidyr::gather(Off, On, key = "Player", value = "OnOff") %>%
+    arrange(match_id, ElapsedTime)
+
+  SubsOff <- Subs %>%
+    filter(Player == "Off") %>%
+    select(-Player, TimeOff = ElapsedTime, player.id = OnOff)
+
+  df <- left_join(df, SubsOff, by = c("player.id", "match_id", "team.id"), suffix = c("", ".S"))
+
+  #Bind in SubsOn
+  SubsOn <- Subs %>%
+    filter(Player == "On") %>%
+    select(-Player, TimeOn = ElapsedTime, player.id = OnOff)
+
+  df <- bind_rows(df, SubsOn) %>%
+    arrange(match_id, team.id, TimeOn)
+
+  #Set TimeOff to GameEnd for all player that played the whole match
+  df <- df %>% left_join(Matches)
+  df <- df %>%
+    mutate(TimeOff = ifelse(is.na(TimeOff), GameEnd, TimeOff))
+
+  df <- df %>%
+    mutate(MinutesPlayed = (TimeOff-TimeOn)/60,
+           TimeOn = TimeOn/60,
+           TimeOff = TimeOff/60,
+           GameEnd = GameEnd/60)
+
+  return(df)
 }
