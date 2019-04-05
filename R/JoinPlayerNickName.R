bc8e8ea3d5afd5f98d538b43a8b3585bfed572e0
YamStats <derrick_yam@brown.edu>
2019-04-04 20:23:49 -0400

    Fixed a potential bug in the code where multiple player ids would be returned augmenting the original data

--- R/JoinPlayerNickName.R
+++ R/JoinPlayerNickName.R
@@ -5,7 +5,10 @@ JoinPlayerNickName <- function(events, username, password){
   lineups <- lineups %>%
     mutate(player_nickname = ifelse(is.na(player_nickname), player_name, player_nickname))
   lineups <- lineups %>%
-    select(player.id = player_id, player.nickname = player_nickname)
+    select(player.id = player_id, player.nickname = player_nickname) %>%
+    group_by(player.id) %>%
+    slice(1) %>%
+    ungroup()
   events <- events %>%
     left_join(lineups)
   return(events)
