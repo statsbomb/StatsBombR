3068723f7f5baf90cb77c5f78c4f2c0d5da51b35
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2020-02-27 14:07:40 +0000

    Update JoinPlayerNickName.R

--- R/JoinPlayerNickName.R
+++ R/JoinPlayerNickName.R
@@ -1,6 +1,6 @@
 JoinPlayerNickName <- function(events, username, password){
   match_ids <- unique(events$match_id)
-  lineups <- alllineups(username, password, match_ids, version = "v2")
+  lineups <- alllineups(username, password, match_ids, version = "v5")
   lineups <- cleanlineups(lineups)
   lineups <- lineups %>%
     mutate(player_nickname = ifelse(is.na(player_nickname), player_name, player_nickname))
