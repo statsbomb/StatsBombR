9d977c8b634ba83613709b4216921730b8f79971
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2020-02-27 14:06:50 +0000

    Update MultiCompEvents.R

--- R/MultiCompEvents.R
+++ R/MultiCompEvents.R
@@ -1,5 +1,5 @@
 ##Multi Competition Events Function.
-MultiCompEvents <- function(username, password, competitionmatrix, version = "v1",
+MultiCompEvents <- function(username, password, competitionmatrix, version = "v5",
                             baseurl = "https://data.statsbombservices.com/api/", parallel = TRUE, cores = detectCores()){
   events <- tibble()
   for(i in 1:dim(competitionmatrix)[1]){
