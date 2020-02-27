fcadbd99cd4ad8e4c96594592cffbb69c97e327a
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2020-02-27 14:10:35 +0000

    Update matchesvector.R

--- R/matchesvector.R
+++ R/matchesvector.R
@@ -1,4 +1,4 @@
-matchesvector <- function(username, password, season_id, competition_id, version = "v1", baseurl = "https://data.statsbombservices.com/api/"){
+matchesvector <- function(username, password, season_id, competition_id, version = "v5", baseurl = "https://data.statsbombservices.com/api/"){
   competition_id <- competition_id
   season_id <- season_id
   matches <- get.matches(username, password, season_id, competition_id, version, baseurl)
