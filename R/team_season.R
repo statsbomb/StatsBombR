c32ca0b1cc78a9e66020b6207ee7afa3cb4ae256
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2021-07-12 12:04:50 +0100

    Update team_season.R

--- R/team_season.R
+++ R/team_season.R
@@ -1,17 +1,17 @@
 team_season = function (username, password, competition_id, season_id, version = "v5", baseurl = "https://data.statsbombservices.com/api/") 
 {
-  events <- tibble()
-  Events.url <- paste0(baseurl, version,"/competitions/",competition_id, "/seasons/", season_id, "/team-stats")
-  raw.events.api <- GET(url = Events.url, authenticate(username, 
+  team_season <- tibble()
+  team_season.url <- paste0(baseurl, version,"/competitions/",competition_id, "/seasons/", season_id, "/team-stats")
+  raw.team_season.api <- GET(url = team_season.url, authenticate(username, 
                                                        password))
-  events.string <- rawToChar(raw.events.api$content)
-  Encoding(events.string) <- "UTF-8"
-  events <- fromJSON(events.string, flatten = T)
-  if (length(events) == 0) {
-    events <- tibble()
+  team_season.string <- rawToChar(raw.team_season.api$content)
+  Encoding(team_season.string) <- "UTF-8"
+  team_season <- fromJSON(team_season.string, flatten = T)
+  if (length(team_season) == 0) {
+    team_season <- tibble()
   }
   else {
     NA
   }
-  return(events)
+  return(team_season)
 }
