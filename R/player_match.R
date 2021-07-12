448fd622941264c45aa02a018579041390a3a4e3
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2021-07-12 12:03:49 +0100

    Update player_match.R

--- R/player_match.R
+++ R/player_match.R
@@ -1,17 +1,17 @@
 player_match = function (username, password, match_id, version = "v5", baseurl = "https://data.statsbombservices.com/api/") 
 {
-  events <- tibble()
-  Events.url <- paste0(baseurl, version,"/matches/",  match_id,"/player-stats")
-  raw.events.api <- GET(url = Events.url, authenticate(username, 
+  player_match <- tibble()
+  player_match.url <- paste0(baseurl, version,"/matches/",  match_id,"/player-stats")
+  raw.player_match.api <- GET(url = player_match.url, authenticate(username, 
                                                        password))
-  events.string <- rawToChar(raw.events.api$content)
-  Encoding(events.string) <- "UTF-8"
-  events <- fromJSON(events.string, flatten = T)
-  if (length(events) == 0) {
-    events <- tibble()
+  player_match.string <- rawToChar(raw.player_match.api$content)
+  Encoding(player_match.string) <- "UTF-8"
+  player_match <- fromJSON(player_match.string, flatten = T)
+  if (length(player_match) == 0) {
+    player_match <- tibble()
   }
   else {
-    events <- events %>% mutate(match_id = match_id)
+    player_match <- player_match %>% mutate(match_id = match_id)
   }
-  return(events)
+  return(player_match)
 }
