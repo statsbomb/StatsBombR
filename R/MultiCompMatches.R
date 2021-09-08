2ae9f576b265638205531fbf0be6edce28562421
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2021-09-08 12:36:17 +0100

    Update MultiCompMatches.R

--- R/MultiCompMatches.R
+++ R/MultiCompMatches.R
@@ -1,5 +1,5 @@
 ##Multi Competition Matches Function.
-MultiCompMatches <- function(username, password, competitionmatrix, version = "v5",
+MultiCompMatches <- function(username, password, competitionmatrix, version = "v6",
                              baseurl = "https://data.statsbombservices.com/api/",
                              remove.deleted.matches = TRUE){
   strt<-Sys.time()
