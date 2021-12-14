a825a425ad5856b71a23e3b3b79f03dc34da6db9
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2021-12-14 16:54:09 +0000

    Create player_season_multicomp.R

---
+++ R/player_season_multicomp.R
@@ -0,0 +1,14 @@
+player_season_multicomp = function (username, password, competitionmatrix, version = "v6", 
+                                  baseurl = "https://data.statsbombservices.com/api/", parallel = TRUE, 
+                                  cores = detectCores()) 
+{
+  events <- tibble()
+  for (i in 1:dim(competitionmatrix)[1]) {
+    player_seasons <- tibble()
+    competition_id <- as.numeric(competitionmatrix[i, 1])
+    season_id <- as.numeric(competitionmatrix[i, 2])
+    player_seasons = player_season(username, password, competition_id, season_id)
+    events <- bind_rows(events, player_seasons)
+  }
+  return(events)
+}
