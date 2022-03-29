c9c1c72d7cb38fe3d4daab831c72fcad5c4a1444
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2022-03-29 13:21:57 +0100

    Update FreeMatches.R

--- R/FreeMatches.R
+++ R/FreeMatches.R
@@ -1,5 +1,5 @@
 FreeMatches <- function(Competitions){
-  print("Whilst we are keen to share data and facilitate research, we also urge you to be responsible with the data. Please register your details on https://www.statsbomb.com/resource-centre and read our User Agreement carefully.")
+  print("Whilst we are keen to share data and facilitate research, we also urge you to be responsible with the data. Please credit StatsBomb as your data source when using the data and visit https://statsbomb.com/media-pack/ to obtain our logos for public use.")
   Matches.df <- tibble()
   for(i in 1:nrow(Competitions)){
     Matches.url <- paste0("https://raw.githubusercontent.com/statsbomb/open-data/master/data/matches/", Competitions$competition_id[i], "/", Competitions$season_id[i], ".json")
