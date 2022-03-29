4db7e11928dfc043e9fdf99c4797f0ebdf292577
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2022-03-29 13:21:41 +0100

    Update FreeCompetitions.R

--- R/FreeCompetitions.R
+++ R/FreeCompetitions.R
@@ -1,5 +1,5 @@
 FreeCompetitions <- function(){
-  print("Whilst we are keen to share data and facilitate research, we also urge you to be responsible with the data. Please register your details on https://www.statsbomb.com/resource-centre and read our User Agreement carefully.")
+  print("Whilst we are keen to share data and facilitate research, we also urge you to be responsible with the data. Please credit StatsBomb as your data source when using the data and visit https://statsbomb.com/media-pack/ to obtain our logos for public use.")
   Competitions.url <- "https://raw.githubusercontent.com/statsbomb/open-data/master/data/competitions.json"
   raw.competitions <- GET(url = Competitions.url)
   competitions.string <- rawToChar(raw.competitions$content)
