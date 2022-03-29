58e4740171ac1c13a9e7c6055a7260199639ec88
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2022-03-29 13:22:08 +0100

    Update StatsBombFree360Events.R

--- R/StatsBombFree360Events.R
+++ R/StatsBombFree360Events.R
@@ -1,5 +1,5 @@
 StatsBombFree360Events <- function(MatchesDF = "ALL", Parallel = T){
-  print("Whilst we are keen to share data and facilitate research, we also urge you to be responsible with the data. Please register your details on https://www.statsbomb.com/resource-centre and read our User Agreement carefully.")
+  print("Whilst we are keen to share data and facilitate research, we also urge you to be responsible with the data. Please credit StatsBomb as your data source when using the data and visit https://statsbomb.com/media-pack/ to obtain our logos for public use.")
   events.df <- tibble()
   
   if(Parallel == T){
