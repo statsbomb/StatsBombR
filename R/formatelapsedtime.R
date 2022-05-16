97c5c5b5b146b647f4e0b55b5d4c90012aa28767
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2022-05-16 12:25:50 +0100

    Update formatelapsedtime.R

--- R/formatelapsedtime.R
+++ R/formatelapsedtime.R
@@ -1,27 +1,24 @@
-formatelapsedtime <- function(df){
-  df <- df %>%
-    mutate(milliseconds = as.numeric(str_extract(timestamp, "[[:digit:]]+$"))) %>%
-    mutate(ElapsedTime = minute*60*1000 + second*1000 + milliseconds)
-
-  periods <- df %>%
+formatelapsedtime <- function (df) {
+  df <- df %>% 
+    mutate(milliseconds = as.numeric(str_extract(timestamp, "[[:digit:]]+$"))) %>% 
+    mutate(ElapsedTime = minute * 60 * 1000 + second * 1000 + milliseconds) %>%
+    ungroup()
+  
+  time_lost <- df %>%
     group_by(match_id, period) %>%
-    summarise(endhalf = max(ElapsedTime)) %>%
-    mutate(period = period + 1)
-
-  firsthalf <- tibble(match_id = unique(periods$match_id),
-                      period = rep(1, length(unique(periods$match_id))),
-                      endhalf =  rep(0, length(unique(periods$match_id))))
-
-  periods <- bind_rows(periods, firsthalf)
-
-  df <- left_join(df, periods) %>%
+    summarise(endhalf = max(ElapsedTime),
+              starthalf = min(ElapsedTime)) %>%
+    mutate(time_lost = dplyr::lag(endhalf)-starthalf,
+           time_lost = ifelse(is.na(time_lost), 0, time_lost), 
+           time_lost = cumsum(time_lost)) %>%
     ungroup() %>%
-    mutate(ElapsedTime = ifelse(period == 1, endhalf + ElapsedTime,
-                                ifelse(period == 2, endhalf + ElapsedTime - (45*60*1000),
-                                       ifelse(period == 3, endhalf + ElapsedTime  - (90*60*1000),
-                                              ifelse(period == 4, endhalf + ElapsedTime  - (105*60*1000),
-                                                     ifelse(period == 5, endhalf + ElapsedTime  - (120*60*1000), NA)))))) %>%
+    select(match_id, period,time_lost)
+  
+  df <- df %>%
+    left_join(time_lost) %>%
+    mutate(ElapsedTime = ElapsedTime + time_lost) %>%
+    select(-time_lost) %>%
     mutate(ElapsedTime = ElapsedTime/1000) %>%
-    select(-endhalf, milliseconds)
+    ungroup()
   return(df)
 }
