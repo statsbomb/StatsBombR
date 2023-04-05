9e6d3779b23da9e731d676d614e816386a35cc2e
EuanDewar <39637446+EuanDewar@users.noreply.github.com>
2023-04-05 19:42:38 +0100

    Update player_season.R

--- R/player_season.R
+++ R/player_season.R
@@ -1,4 +1,4 @@
-player_season = function (username, password, competition_id, season_id, version = "v5", baseurl = "https://data.statsbomb.com/api/") 
+player_season = function (username, password, competition_id, season_id, version = "v4", baseurl = "https://data.statsbomb.com/api/") 
 {
   player_season <- tibble()
   player_season.url <- paste0(baseurl, version,"/competitions/",competition_id, "/seasons/", season_id, "/player-stats")
